97b8f5b4283f8034d66db83eaf69e50c51cde99d
gojaebeom <const.gjb@gmail.com>
2021-05-08 14:48:15 +0900

    update

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 const express = require("express");
 const app = express();
 
-app.use(express.json());
 app.set("views","./public/views");
 app.set("view engine" , "ejs");
 app.use(express.static(__dirname + '/public/static'));
 
+app.use(express.json());
+
 app.get("/", ( req, res ) => {
-    console.log("map index");
     res.render("index");
 });
 
-app.listen(3000, () => console.log("server is running on 3000 port "));
+app.listen(3000, () => console.log("server is running on 3000 port"));
